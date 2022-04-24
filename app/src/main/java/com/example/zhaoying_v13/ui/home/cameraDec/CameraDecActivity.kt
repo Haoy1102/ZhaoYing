228a8b6fe0a7f7ff714908e9b2851159b77f6b96
郝阳 <8392521+hao-yang11@user.noreply.gitee.com>
2022-04-24 22:33:35 +0800

    0.0.2 CameraActivity实现

--- app/src/main/java/com/example/zhaoying_v13/ui/home/cameraDec/CameraDecActivity.kt
+++ app/src/main/java/com/example/zhaoying_v13/ui/home/cameraDec/CameraDecActivity.kt
@@ -1,14 +1,119 @@
 package com.example.zhaoying_v13.ui.home.cameraDec
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import com.example.zhaoying_v13.R
+import android.provider.MediaStore
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.zhaoying_v13.databinding.ActivityCameraDecBinding
+
 
 class CameraDecActivity : AppCompatActivity() {
 
+    companion object {
+        const val REQUEST_VIDEO_CAPTURE = 1
+    }
+    private lateinit var binding: ActivityCameraDecBinding
+    private var imageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_camera_dec)
+        binding = ActivityCameraDecBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        dispatchTakeVideoIntent()
 
     }
+
+    private fun dispatchTakeVideoIntent() {
+
+
+
+
+        val values = ContentValues()
+        values.put(MediaStore.Video.Media.TITLE, "MyVideo")
+        values.put(
+            MediaStore.Video.Media.DESCRIPTION,
+            "Video taken on " + System.currentTimeMillis()
+        )
+        imageUri = contentResolver.insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values)
+
+//        Intent(MediaStore.ACTION_VIDEO_CAPTURE).
+//        putExtra(MediaStore.EXTRA_OUTPUT, imageUri).also { takeVideoIntent ->
+//            takeVideoIntent.resolveActivity(packageManager)?.also {
+//                startActivityForResult(takeVideoIntent, Companion.REQUEST_VIDEO_CAPTURE)
+//
+//            }
+//        }
+        Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { takeVideoIntent ->
+            takeVideoIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takeVideoIntent, Companion.REQUEST_VIDEO_CAPTURE)
+
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            val videoUri: Uri? = data?.data
+            Log.i("TAGCamera", videoUri.toString())
+            binding.videoView.setVideoURI(videoUri)
+            Log.i("TAGCamera", getFilePathByUri(videoUri)!!)
+
+            binding.videoView.start()
+
+//            Log.i("TAGCamera", intent.data.toString())
+//
+//            val selectedImage = intent.data
+//            val filePathColumns = arrayOf(MediaStore.Images.Media.DATA)
+//            val c: Cursor =
+//                selectedImage?.let {
+//                    this.contentResolver.query(
+//                        it,
+//                        filePathColumns,
+//                        null,
+//                        null,
+//                        null
+//                    )
+//                }!!
+//            c.moveToFirst()
+//            val columnIndex = c.getColumnIndex(filePathColumns[0])
+//            imageUri = Uri.fromFile()c.getString(columnIndex)
+//
+//            imageUri=MediaStore.Video.Media.getContentUri(imageUri.toString())
+
+
+
+//            Log.i("TAGCamera", imageUri.toString())
+//            binding.videoView.setVideoURI(imageUri)
+        }
+    }
+
+    fun getFilePathByUri(uri: Uri?): String? {
+        val selectedImage = uri
+        val filePathColumns = arrayOf(MediaStore.Images.Media.DATA)
+        val c: Cursor =
+            selectedImage?.let {
+                this.contentResolver.query(
+                    it,
+                    filePathColumns,
+                    null,
+                    null,
+                    null
+                )
+            }!!
+        c.moveToFirst()
+        val columnIndex = c.getColumnIndex(filePathColumns[0])
+        val imagePath = c.getString(columnIndex)
+        return imagePath
+    }
+
+
 }
--- app/src/main/java/com/example/zhaoying_v13/ui/home/cameraDec/CameraDecFragment.kt
+++ app/src/main/java/com/example/zhaoying_v13/ui/home/cameraDec/CameraDecFragment.kt
@@ -1,14 +1,21 @@
 package com.example.zhaoying_v13.ui.home.ui.camaraDec
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.zhaoying_v13.R
+import com.example.zhaoying_v13.databinding.FragmentCamareDecBinding
+import com.example.zhaoying_v13.databinding.SelectFragmentBinding
 
 class CameraDecFragment : Fragment() {
+
+    private var _binding: FragmentCamareDecBinding? = null
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = CameraDecFragment()
@@ -16,17 +23,36 @@ class CameraDecFragment : Fragment() {
 
     private lateinit var viewModel: CameraDecViewModel
 
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_camare_dec, container, false)
+        _binding = FragmentCamareDecBinding.inflate(inflater, container, false)
+
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(CameraDecViewModel::class.java)
         // TODO: Use the ViewModel
+
+
+
+
     }
+
+//    const val REQUEST_VIDEO_CAPTURE = 1
+//
+//    private fun dispatchTakeVideoIntent() {
+//        Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { takeVideoIntent ->
+//            takeVideoIntent.resolveActivity()?.also {
+//                startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE)
+//            }
+//        }
+//    }
 
 }
